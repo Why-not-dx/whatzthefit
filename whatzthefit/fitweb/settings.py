fc095c05ab4571806c9df71d6696585ee94b4e25
Tony <anthony.destremx@gmail.com>
2024-05-01 18:45:01 +0200

    django-environ test

--- whatzthefit/fitweb/settings.py
+++ whatzthefit/fitweb/settings.py
@@ -12,23 +12,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import json
-from dotenv import load_dotenv
+import environ
 
-load_dotenv()
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_KEY")
+# SECRET_KEY = os.getenv("DJANGO_KEY")
+SECRET_KEY = env("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = env('DEBUG')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
@@ -40,18 +44,18 @@ EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.hostinger.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("DJANGO_EMAIL")
-EMAIL_HOST_PASSWORD = os.getenv("MAIL_PASS")
+EMAIL_HOST_USER = env("DJANGO_EMAIL")
+EMAIL_HOST_PASSWORD = env("MAIL_PASS")
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
 ADMINS = [
-    ("Anthony", os.getenv("MY_EMAIL"))
+    ("Anthony", env("MY_EMAIL"))
     ]
 
 # Application definition
@@ -113,9 +117,9 @@ WSGI_APPLICATION = 'fitweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASS"),
+        "NAME":env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASS"),
         "HOST": "db",
         "PORT": "3306",
         "OPTIONS": {
